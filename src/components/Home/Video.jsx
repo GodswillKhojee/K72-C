188cbeceae9d4f0a665afad7f42b8acca73296fa
Gods Will Khojee <ardiumwing@gmail.com>
2026-04-03 07:17:23 +0530

    agency page

--- src/components/Home/Video.jsx
+++ src/components/Home/Video.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Video = () => {
   return (
     <div className='h-full w-full'>
-      <video className='h-full w-full object-cover' autoPlay muted loop src="https://download-video-ak.vimeocdn.com/v3-1/playback/36bc59b8-6671-4358-abc2-15555fc6ae59/69496b2d?__token__=st=1775172141~exp=1775175741~acl=%2Fv3-1%2Fplayback%2F36bc59b8-6671-4358-abc2-15555fc6ae59%2F69496b2d%2A~hmac=1f3efddb0250fb8bdeab68b7b714cdfa55f9372fc5c2ecc6405696244168f5d6&r=dXMtZWFzdDE%3D"></video>
+      <video className='h-full w-full object-cover' autoPlay muted loop src="/Video/69496b2d.mp4"></video>
     </div>
   )
 }
--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -1,17 +1,60 @@
-import React from 'react'
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all';
+import React, { useRef } from 'react'
 
 const Agence = () => {
+    const imagesDivRef = useRef(null);
+    const imageRef = useRef(null);
+
+    const imageArray = [
+        "Images/Carl_480x640.jpg",
+        "Images/Olivier_480x640.jpg" ,
+        "Images/ChantalG_480x640.jpg",
+        "Images/Michele_480X640.jpg",
+        "Images/MEL_480X640.jpg",
+        "Images/CAMILLE_480X640_2.jpg",
+        "Images/MEGGIE_480X640_2.jpg",
+        "Images/joel_480X640_3.jpg",
+];
+
+    gsap.registerPlugin(ScrollTrigger);
+    
+    useGSAP(function(){
+        gsap.to(imagesDivRef.current,{
+            scrollTrigger:{
+                trigger:imagesDivRef.current,
+                markers:true,
+                // start:'top 21.8%',
+                // end: 'top -140%',
+                pin: true,
+                scrub: true,
+                onUpdate: (ele)=>
+                {
+                    let imageIndex
+                    if(ele.progress < 1)
+                    {
+                        imageIndex =Math.floor(ele.progress * imageArray.length)
+                    }
+                    else{
+                        imageIndex = imageArray.length -1;
+                    }
+                    imageRef.current.src = imageArray[imageIndex]
+                }
+            }
+        })
+    })
   return (
     <div>
         <div className='section1'>
-        <div className='h-[20vw] rounded-3xl overflow-hidden w-[15vw] absolute top-40 left-[30vw] bg-red-500'>
-            <img className='h-full w-full object-cover' src="https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7" alt="" />
+        <div ref={imagesDivRef} className='h-[20vw] rounded-3xl overflow-hidden w-[15vw] absolute top-40 left-[30vw]'>
+            <img ref={imageRef} className='h-full w-full object-cover' src="/Images/Carl_480x640.jpg" alt="" />
         </div>
         <div className='relative font-[font2] '>
       <div className='mt-[55vh]'>
         <h1 className='text-[20vw] uppercase leading-[18vw] text-center'>SEVEN7Y <br /> TWO</h1>
       </div>
-      <div className='pl-[40%]'>
+      <div className='pl-[40%] '>
         <p className='text-6xl'>
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; We’re inquisitive and open-minded, and we make sure creativity crowds out ego from every corner. A brand is a living thing, with values, a personality and a story. If we ignore that, we can achieve short-term success, but not influence that goes the distance. We bring that perspective to every brand story we help tell.
         </p>
